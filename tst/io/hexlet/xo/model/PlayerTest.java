f3d55751153d882be2ca17961d9460d25446463a
vov1k <vov1k.15@gmail.com>
2016-02-13 15:20:09 +0600

    test player

--- tst/io/hexlet/xo/model/PlayerTest.java
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -8,7 +8,12 @@ public class PlayerTest {
 
     @Test
     public void testGetName() throws Exception {
+        final String testValue = "Slava";
+        final String expectedValue = testValue;
+        final Player player = new Player(testValue, null);
 
+        final String actialValue = player.getName();
+        assertEquals(expectedValue, actialValue);
     }
 
     @Test
