5ab859c15357b6ed77ab8eb6cc95e4b12278f9c6
vov1k <vov1k.15@gmail.com>
2016-02-14 16:17:30 +0500

    move controller

--- src/io/hexlet/xo/controller/MoveController.java
+++ src/io/hexlet/xo/controller/MoveController.java
@@ -1,8 +1,21 @@
 package io.hexlet.xo.controller;
 
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exception.AlreadyOccupiedException;
+import io.hexlet.xo.model.exception.InvalidPointException;
+
+import java.awt.*;
+
 public class MoveController {
 
-    public void applyFigure() {
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws AlreadyOccupiedException, InvalidPointException {
+        if(field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
 
+        field.setFigure(point, figure);
     }
 }
--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -90,19 +90,4 @@ public class FieldTest {
 
         assertEquals(inputFigure, actualFigure);
     }
-
-    @Test
-    public void testSetFigureWhenAlreadyOccupied() throws Exception {
-        final Field field = new Field();
-        final Point inputPoint = new Point(0,0);
-        final Figure inputFigure = Figure.O;
-
-        field.setFigure(inputPoint, inputFigure);
-        try {
-            field.setFigure(inputPoint, inputFigure);
-            fail();
-        } catch (final AlreadyOccupiedException e) {
-
-        }
-    }
 }
