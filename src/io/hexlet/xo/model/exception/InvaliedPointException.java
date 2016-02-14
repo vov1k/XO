ffcfeb3d08575f1727bdb4c25865194e3d32a429
vov1k <vov1k.15@gmail.com>
2016-02-14 14:42:24 +0500

    exception

---
+++ src/io/hexlet/xo/model/exception/InvaliedPointException.java
@@ -0,0 +1,4 @@
+package io.hexlet.xo.model.exception;
+
+public class InvaliedPointException extends XOException {
+}
---
+++ src/io/hexlet/xo/model/exception/XOException.java
@@ -0,0 +1,4 @@
+package io.hexlet.xo.model.exception;
+
+public abstract class XOException extends Exception {
+}
