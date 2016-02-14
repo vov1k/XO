b141982c1a9ac6ee9466893fd5fe464263ca67cf
vov1k <vov1k.15@gmail.com>
2016-02-14 14:25:54 +0500

    field test

---
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -0,0 +1,34 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    @Test
+    public void testGetSize() throws Exception {
+        final Field field = new Field[];
+
+        assertEquals(3, field.getSize());
+    }
+
+    @Test
+    public void testGetFigure() throws Exception {
+
+    }
+
+    @Test
+    public void testSetFigure() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = new Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(inputPoint, actualFigure);
+    }
+}
