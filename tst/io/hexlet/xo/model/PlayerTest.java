bf3f5ccd17ca5ee15f2c01f2ad267562c2a713c9
vov1k <vov1k.15@gmail.com>
2016-02-14 13:37:59 +0500

    test

--- tst/io/hexlet/xo/model/PlayerTest.java
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -9,15 +9,24 @@ public class PlayerTest {
     @Test
     public void testGetName() throws Exception {
         final String testValue = "Slava";
-        final String expectedValue = testValue;
+        final String ExpectedValue = testValue;
+
         final Player player = new Player(testValue, null);
 
-        final String actialValue = player.getName();
-        assertEquals(expectedValue, actialValue);
+        final String actualValue = player.getName();
+
+        assertEquals(ExpectedValue, actualValue);
     }
 
     @Test
     public void testGetFigure() throws Exception {
+        final Figure testValue = Figure.X;
+        final Figure ExpectedValue = testValue;
 
+        final Player player = new Player(null, testValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(ExpectedValue, actualValue);
     }
 }
